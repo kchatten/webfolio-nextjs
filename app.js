a78cd5c69938346b0e1a86203d8eef7a421aada7
kchatten <kyalchatten@gmail.com>
2024-06-27 18:48:52 -0400

    Added CORS Middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const next = require('next');
 const path = require('path');
+const cors = require('cors');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev }); // Initialize the application with the appropriate configuration
@@ -14,6 +15,13 @@ app.prepare() // A Serverside method that ensures dependencies are loaded before
   // Serve static files from the 'public' directory
   server.use(express.static(path.join(__dirname, 'public')));
 
+  // Configure CORS to allow requests from any origin
+  server.use(cors({
+    origin: '*', // You can customize this to allow specific origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }));
 
   // TODO: Read more into the handler to expand on documentation
   // Handle requests for Next.js pages

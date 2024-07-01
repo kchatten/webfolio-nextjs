6b76052b35b316c5e1b9b1592e21299a33e0fa88
kchatten <kyalchatten@gmail.com>
2024-07-01 19:10:02 -0400

    Updated styling

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-      return config;
-    },
   };
   
   export default nextConfig;
